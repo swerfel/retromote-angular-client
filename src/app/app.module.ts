3555950f7047e22bde1b178697f2c1dc280fe96f
Sergej Werfel <email@sergej-werfel.de>
2018-01-10 21:47:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule
