f35a64ac3296449cb2dc8245b8a7b9173972a137
Sergej Werfel <email@sergej-werfel.de>
2018-01-10 22:43:50 +0100

    show first postit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
+import { PostitComponent } from './postit/postit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent
+    ChartComponent,
+    PostitComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/postit/postit.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Dimension } from '../dimension';
+
+@Component({
+  selector: '[app-postit]',
+  templateUrl: './postit.component.html',
+  styleUrls: ['./postit.component.css']
+})
+export class PostitComponent implements OnInit {
+  text: string = "PostIt text";
+  size: Dimension = {
+    width: 150,
+    height: 100
+  };
+  location: Position = {
+    x: 10,
+    y: 5
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
