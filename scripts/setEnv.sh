bbc8641fc7253040636f7e94021a5041565e5b5f
Sergej Werfel <email@sergej-werfel.de>
2018-04-04 21:52:52 +0200

    remove unused scripts

--- scripts/setEnv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -ri "s/FIREBASE_API_KEY/$FIREBASE_API_KEY/" src/environments/environment.prod.ts
-sed -ri "s/FIREBASE_MESSAGING_SENDER_ID/$FIREBASE_MESSAGING_SENDER_ID/" src/environments/environment.prod.ts
