86d3f692cf894d26e100b2a22160c74d105dccd2
Sergej Werfel <sergej.werfel@andrena.de>
2018-04-04 15:47:25 +0200

    try to configure firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: $FIREBASE_API_KEY,
+    apiKey: FIREBASE_API_KEY,
     authDomain: "retromote.firebaseapp.com",
     databaseURL: "https://retromote.firebaseio.com",
     projectId: "retromote",
     storageBucket: "retromote.appspot.com",
-    messagingSenderId: $FIREBASE_MESSAGING_SENDER_ID
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID
   }
 };
