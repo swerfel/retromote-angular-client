6765afde3329dcc6da3a938e33159d37a0757ce3
Sergej Werfel <sergej.werfel@andrena.de>
2018-04-04 15:28:36 +0200

    Try to use environment variables in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: $FIREBASE_API_KEY,
+    authDomain: "retromote.firebaseapp.com",
+    databaseURL: "https://retromote.firebaseio.com",
+    projectId: "retromote",
+    storageBucket: "retromote.appspot.com",
+    messagingSenderId: $FIREBASE_MESSAGING_SENDER_ID
+  }
 };
